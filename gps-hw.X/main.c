a0b6f70666913aab6e199cf1117295044559e892
Derek Pecka <derek@pecka.io>
2025-09-01 22:12:51 -0700

    Toggle activity pin each loop

--- gps-hw.X/main.c
+++ gps-hw.X/main.c
@@ -46,6 +46,80 @@
   Section: Included Files
 */
 #include "mcc_generated_files/system.h"
+#include "mcc_generated_files/uart1.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#define FCY (_XTAL_FREQ/2)
+#include <libpic30.h>
+
+#define HTTP_URL "http://example.com/position"
+
+static void BG95_SendString(const char *s)
+{
+    while (*s)
+    {
+        while (!UART1_IsTxReady());
+        UART1_Write(*s++);
+    }
+}
+
+static void BG95_ReadLine(char *buffer, size_t len)
+{
+    size_t i = 0;
+
+    while (i < len - 1)
+    {
+        while (!UART1_IsRxReady());
+        char c = UART1_Read();
+        buffer[i++] = c;
+        if (c == '\n')
+        {
+            break;
+        }
+    }
+
+    buffer[i] = '\0';
+}
+
+static void BG95_GetPosition(char *pos, size_t len)
+{
+    BG95_SendString("AT+QGPS=1\r\n");
+    __delay_ms(1000);
+    BG95_SendString("AT+QGPSLOC=2\r\n");
+    BG95_ReadLine(pos, len);
+}
+
+static void BG95_PostPosition(const char *pos)
+{
+    char cmd[128];
+    char body[128];
+
+    snprintf(body, sizeof(body), "{\"pos\":\"%s\"}", pos);
+
+    BG95_SendString("AT+QHTTPCFG=\"contenttype\",\"application/json\"\r\n");
+    __delay_ms(100);
+    snprintf(cmd, sizeof(cmd), "AT+QHTTPURL=%u,80\r\n", (unsigned)strlen(HTTP_URL));
+    BG95_SendString(cmd);
+    __delay_ms(100);
+    BG95_SendString(HTTP_URL);
+    BG95_SendString("\r\n");
+    __delay_ms(100);
+    snprintf(cmd, sizeof(cmd), "AT+QHTTPPOST=%u,80,80\r\n", (unsigned)strlen(body));
+    BG95_SendString(cmd);
+    __delay_ms(100);
+    BG95_SendString(body);
+    BG95_SendString("\r\n");
+}
+
+static void SleepOneMinute(void)
+{
+    RCONbits.SWDTEN = 1;
+    __builtin_clrwdt();
+    Sleep();
+    RCONbits.SWDTEN = 0;
+}
 
 /*
                          Main application
@@ -55,9 +129,20 @@ int main(void)
     // initialize the device
     SYSTEM_Initialize();
 
+    // Configure RA4 as digital output for loop activity indication
+    TRISAbits.TRISA4 = 0;
+    LATAbits.LATA4 = 0;
+
     while (1)
     {
-        // Add your application code
+        char position[64];
+
+        // Toggle RA4 each reporting cycle
+        LATAbits.LATA4 ^= 1;
+
+        BG95_GetPosition(position, sizeof(position));
+        BG95_PostPosition(position);
+        SleepOneMinute();
     }
 
     return 1;
